12742661f98c22ff6631aedc728de8d340222029
twanz <clockcoing1>
2023-04-18 15:42:39 -0400

    v1

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from .craigslist import (
+    CraigslistCommunity,
+    CraigslistEvents,
+    CraigslistForSale,
+    CraigslistGigs,
+    CraigslistHousing,
+    CraigslistJobs,
+    CraigslistResumes,
+    CraigslistServices,
+)
+
+__all__ = [
+    "CraigslistCommunity",
+    "CraigslistEvents",
+    "CraigslistForSale",
+    "CraigslistGigs",
+    "CraigslistHousing",
+    "CraigslistJobs",
+    "CraigslistResumes",
+    "CraigslistServices",
+]
---
+++ browser.py
@@ -0,0 +1,71 @@
+import threading
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class CraigslistBrowser:
+    OPTIONS = Options()
+    # OPTIONS.add_argument("--headless")
+    OPTIONS.add_argument("--no-sandbox")
+    OPTIONS.add_argument("--disable-gpu")
+    OPTIONS.add_argument("--disable-infobars")
+    OPTIONS.add_argument("--disable-extensions")
+    OPTIONS.add_argument("--disable-dev-shm-usage")
+    OPTIONS.add_argument("--disable-application-cache")
+    OPTIONS.add_argument("--disable-setuid-sandbox")
+
+    DRIVER = None
+    last_called = time.time()
+    waiting = False
+
+    @classmethod
+    def set_driver(cls):
+        cls.DRIVER = webdriver.Chrome(
+            executable_path="/opt/homebrew/bin/chromedriver", chrome_options=cls.OPTIONS
+        )
+
+    @classmethod
+    def visit(cls, url):
+        cls.last_called = time.time()
+        if not cls.DRIVER:
+            cls.set_driver()
+
+        cls.DRIVER.get(url)
+
+    @classmethod
+    def show_source(cls, wait=False):
+        if wait:
+            cls.waiting = True
+            try:
+                WebDriverWait(cls.DRIVER, 10).until(
+                    EC.presence_of_element_located(
+                        (By.CSS_SELECTOR, "#search-results-page-1 > ol > div")
+                    )
+                )
+            except Exception:
+                cls.waiting = False
+                return
+
+        data = cls.DRIVER.execute_script("return document.documentElement.outerHTML")
+
+        cls.waiting = False
+        return data
+
+    @classmethod
+    def quit(cls):
+        if not cls.DRIVER:
+            return
+
+        cls.DRIVER.quit()
+        cls.DRIVER = None
+
+    @classmethod
+    def check_activity(cls):
+        if (time.time() - cls.last_called >= 5) and not cls.waiting:
+            cls.quit()
+        threading.Timer(5, cls.check_activity).start()
